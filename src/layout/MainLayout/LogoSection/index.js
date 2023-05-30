3e677b980fc28ce5fa9d917218935ccc2fbd52d8
triphan25 <tri.phan@ebst.tech>
2023-05-30 17:34:18 +0700

    update
    update

---
+++ src/layout/MainLayout/LogoSection/index.js
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+// material-ui
+import { ButtonBase } from '@mui/material';
+
+// project imports
+import config from 'config';
+// import Logo from 'ui-component/Logo';
+import Logo from './../../../assets/images/bigLogo.png';
+import { MENU_OPEN } from 'store/actions';
+
+// ==============================|| MAIN LOGO ||============================== //
+
+const LogoSection = () => {
+  const defaultId = useSelector((state) => state.customization.defaultId);
+  const dispatch = useDispatch();
+  return (
+    <ButtonBase disableRipple onClick={() => dispatch({ type: MENU_OPEN, id: defaultId })} component={Link} to={config.defaultPath}>
+      {/* <Logo /> */}
+      <img src={Logo} alt="" height={100} />
+    </ButtonBase>
+  );
+};
+
+export default LogoSection;
